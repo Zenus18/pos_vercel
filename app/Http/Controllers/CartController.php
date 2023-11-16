951d86ce7ecce4e08229b4c86ca6bf132c5a59b8
Zenus18 <nuganuca17@gmail.com>
2023-11-16 14:57:43 +0700

    trying function

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Cart;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CartController extends Controller
 {
@@ -12,7 +13,55 @@ class CartController extends Controller
      */
     public function index()
     {
-        //
+    try {
+        $carts = Cart::whereNull('transaction_id')->orderBy("id","asc")->get();
+        
+        $sub_total = 0;
+        $discount = 0;
+        $total = 0;
+        
+        foreach ($carts as $calculate) {
+            $total +=  $calculate->total;
+            $sub_total +=  $calculate->sub_total;
+        }
+
+        $discount = $sub_total - $total;
+        $to_be_paid = [
+            "sub_total"=> $sub_total,
+            "discount"=> $discount,
+            "total"=> $total,
+        ];
+
+        return response()->json([
+                'status' => 200,
+                'description' => 'Berhasil ditampilkan',
+                'to_be_paid' => $to_be_paid,
+                'data' => $carts,
+            ]);
+        } catch (\Throwable $th) {
+            if ($th instanceof ModelNotFoundException) {
+                return response()->json([
+                    'error'=> [
+                        'status'=> 404,
+                        'description'=> 'Data tidak ditemukan',
+                    ]
+                ], 404);
+            } else if ($th ) {
+                return response()->json([
+                    'error'=> [
+                        'status'=> 500,
+                        'description'=> 'Terjadi kesalahan pada server',
+                    ]
+                ], 500);
+            } else {
+                return response()->json([
+                    'error'=> [
+                        'status'=> 500,
+                        'description'=> 'Terjadi kesalahan pada server',
+                    ]
+                ], 500);
+            }
+        }   
     }
 
     /**
@@ -28,7 +77,29 @@ class CartController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Validator::make($request->all(), [
+            'qty' =>'required',
+        ])->validate();
+        $transaction_id = null;
+        $creator_id = 1;
+        $product_id = 1;
+        $price = 15000;
+        $qty = $request->input('qty'); 
+        $sub_total = $price * $qty;
+        $discount = 0;
+        $total = $sub_total - $discount;
+
+        $addToCart = Cart::create([
+            'transaction_id' => $transaction_id,
+            'creator_id'=> $creator_id,
+            'product_id'=> $product_id,
+            'qty'=> $qty,
+            'sub_total'=> $sub_total,
+            'discount'=> $discount,
+            'total'=> $total,
+        ]);
+
+        return response()->json($addToCart, 200);
     }
 
     /**
